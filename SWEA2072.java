1cf57384ff9c86f50d24cf603489eb40bb357c3b
김평열 <yeolpyeong@gmail.com>
2019-02-21 00:00:19 +0900

    홀수만 더하기

---
+++ SWEA2072.java
@@ -0,0 +1,29 @@
+
+/*
+ * 홀수만 더하기
+ * https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QSEhaA5sDFAUq&categoryId=AV5QSEhaA5sDFAUq&categoryType=CODE
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA2072 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			int sum = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			while (st.hasMoreTokens()) {
+				int number = Integer.parseInt(st.nextToken());
+				if (number % 2 == 1) {
+					sum += number;
+				}
+			}
+
+			System.out.printf("#%d %d \n", t, sum);
+		}
+	}
+}
