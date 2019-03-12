7d6c8fc550dfd43533370185c48cd07ad0a49b2a
김평열 <yeolpyeong@gmail.com>
2019-03-12 22:08:36 +0900

    홀수만 더하기

--- SWEA2072.java
+++ SWEA2072.java
@@ -5,12 +5,11 @@
  */
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class SWEA2072 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
