ed2dc31791585ea862e04da11e3a8d1b89a626d8
김평열 <yeolpyeong@gmail.com>
2019-03-12 22:03:58 +0900

    줄기세포배양

--- SWEA5653.java
+++ SWEA5653.java
@@ -5,7 +5,6 @@
  */
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -98,7 +97,7 @@ public class SWEA5653 {
 		}
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
